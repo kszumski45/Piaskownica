9b8e9cbc96e0abcd15fec178f58f051f35495376
karol <k.szumski45@gmail.com>
2016-02-29 21:16:30 +0100

    Utworzylem nowy branch?

--- Main.java
+++ Main.java
@@ -7,5 +7,6 @@ public class Main {
         Testowa a = new Testowa();
         System.out.println("Podobno będzie działać");
         System.out.println(a.prosze_dodac(3,4));
+        System.out.println("Napiszmy jeszcze coś innego.");
     }
 }
