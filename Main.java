8527939f9f9755144b6d59400a9b82ca50c74fe3
WoKak <Michal@Michas>
2016-02-29 21:02:35 +0100

    Psuję

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package pl.piopw;
 
 public class Main {
 
-    public static main(String[] args) {
+    public static void main(String[] args) {
 	// write your code here
         Testowa a = new Testowa();
         System.out.println("Podobno będzie działać");
--- Testowa.java
+++ Testowa.java
@@ -3,8 +3,8 @@ package pl.piopw;
 /**
  * Created by carl on 25.02.2016.
  */
-public class int Testowa {
+public class Testowa {
     public int prosze_dodac(int a, int b){
-        return a + b;
+    return a + b;
     }
 }
