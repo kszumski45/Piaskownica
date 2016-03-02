af559c6fa4e43313602515fcf2029b9a9973588e
WoKak <Michal@Michas>
2016-03-02 16:59:46 +0100

    moja wersja

---
+++ src/Dodawanie.java
@@ -0,0 +1,7 @@
+
+public class Dodawanie {
+	public int Add(int x, int y) {
+		return x + y;
+	}
+	
+}
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+
+public class Test {
+	public static void main(String[] args) {
+	Dodawanie suma = new Dodawanie();
+	System.out.println(suma.Add(4, 5));
+	}
+}
